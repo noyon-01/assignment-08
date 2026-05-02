e770a1a582ec2943f9e7758886e353cb90dcb7eb
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-05-02 14:11:16 +0600

    summer tips section is added

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import TopProducts from "@/components/TopProducts";
+import SummerTips from "@/components/SummerTips";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <TopProducts />
+      <SummerTips />
     </div>
   );
 }
---
+++ src/components/SummerTips.jsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import water from "@/assets/water.jpg";
+import creame from "@/assets/creame.jpg";
+import cap from "@/assets/cap.jpg";
+
+export default function SummerTips() {
+  return (
+    <div className="container mx-auto bg-[#EFE3CA] my-20 py-10 px-30 rounded-xl">
+      <h1 className="text-4xl font-bold mb-10">Summer Care Tips</h1>
+      <div className="grid grid-cols-3 gap-10 text-center">
+        <div className="bg-base-200 rounde-xl shadow p-10 rounded-xl">
+          <Image
+            className="rounded-xl"
+            src={water}
+            alt="water"
+            height={100}
+            width={400}
+          />
+          <h2 className="text-xl font-bold mt-3">Stay Hydrated</h2>
+          <p className="text-md text-gray-500">
+            Drink 8+ glasses of water daily to beat the heat
+          </p>
+        </div>
+        <div className="bg-base-200 rounde-xl shadow p-10 rounded-xl">
+          <Image
+            className="rounded-xl"
+            src={creame}
+            alt="creame"
+            height={100}
+            width={400}
+          />
+          <h2 className="text-xl font-bold mt-3">Use SPF Daily</h2>
+          <p className="text-md text-gray-500">
+            Apply SPF 30+ sunscreen every 2 hours outdoors
+          </p>
+        </div>
+        <div className="bg-base-200 rounde-xl shadow p-10 rounded-xl">
+          <Image
+            className="rounded-xl"
+            src={cap}
+            alt="cap"
+            height={100}
+            width={400}
+          />
+          <h2 className="text-xl font-bold mt-3">Cover Up</h2>
+          <p className="text-md text-gray-500">
+            Wear wide hats & UV protective clothing outside
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
