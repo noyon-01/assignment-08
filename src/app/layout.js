527577ae69e1f404e4fa3ce3bebe3745a767a817
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-05-02 13:42:11 +0600

    db.json is added and layout customize

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
+import { ToastContainer } from "react-toastify";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const outfit = Outfit({
   subsets: ["latin"],
 });
 
@@ -18,11 +13,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" data-theme="light" className={`h-full antialiased`}>
+      <body className={`${outfit.className} min-h-full flex flex-col`}>
+        <main className="container mx-auto">{children}</main>
+        <ToastContainer />
+      </body>
     </html>
   );
 }
